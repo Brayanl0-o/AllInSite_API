9338a19d0502d2ff3a84c13b3ff68eec05c5b120
Brayan <brayantandap@gmail.com>
2023-10-11 01:01:29 -0500

    fixed > auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,15 @@
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const User = require('../models/user')
+const config = require('../config')
 
-const userControllers={
+const controllerAuth={
     //Controlador para la creacion de Usuarios & Administradores
     signup: async (req,res) =>{
         try{
         const {firstName, lastName,userImg, email, password,phoneNumber,country,years, descriptionUser} = req.body
 
-        const userName = `${names} ${surNames}`;
+        const userName = `${firstName} ${lastName}`;
         const userRegis = new User({
             userName,
             firstName, 
@@ -29,8 +30,8 @@ const userControllers={
         })
         res.status(200).json({token, savedUser})
     }catch(error){
-        return res.status(500).json(error.message)
+        return res.status(500).json({error:"Error interno del servidor", details: error.message})
     }
     },  
 }
-module.exports = userControllers
+module.exports = controllerAuth
