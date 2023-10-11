4ba457d0e21c7b11869e51661baea709c911d724
Brayan <brayantandap@gmail.com>
2023-10-10 22:56:35 -0500

    Fixed > userModel

--- models/user.js
+++ models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const Schema = mongoose.Schema
 
-const Schema = mongoose.Schema({
+const userSchema = new Schema({
     userName: {
         type:String,
         maxLength: 55
@@ -60,8 +62,8 @@ const Schema = mongoose.Schema({
 
     // Encriptado de contraseña
     userSchema.statics.encryptPassword = async(password)=>{
-        const salt = await bycrypt.genSalt(10)
-        return await bycrypt.hash(password, salt)
+        const salt = await bcrypt.genSalt(10)
+        return await bcrypt.hash(password, salt)
     }
 
     //Comparar antes de comenzar
