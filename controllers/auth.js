b7821e1e8cb722166dd3bc2ca9e1b90f97b19be3
Brayan <brayantandap@gmail.com>
2023-10-13 16:23:36 -0500

    Added changePassword

--- controllers/auth.js
+++ controllers/auth.js
@@ -22,48 +22,48 @@ const controllerAuth={
         const userName = `${firstName} ${lastName}`;
         const userRegis = new User({
             userName,
-            firstName, 
+            firstName,
             lastName,
-            userImg, 
-            email, 
+            userImg,
+            email,
             password,
             phoneNumber,
             country,
-            years, 
+            years,
             descriptionUser
         })
         // Asigna un rol al usuario
         userRegis.roles = ['usuario'];
-        
+
         const savedUser= await userRegis.save()
 
         const token = jwt.sign({id: savedUser._id}, config.SECRET,{
-            expiresIn: 86400 //tiempo de que tarda en expirar el token (cada 24h) 
+            expiresIn: 86400 //tiempo de que tarda en expirar el token (cada 24h)
         })
         res.status(200).json({token, savedUser})
       }catch(error){
           return res.status(500).json({error:"Error interno del servidor", details: error.message})
       }
-    }, 
+    },
     login: async (req, res) => {
         try {
           const userFound = await User.findOne({ email: req.body.email });
-      
+
           if (!userFound) {
             return res.status(400).json({ message: 'Usuario no encontrado' });
           }
-      
+
           const isPasswordValid = await User.comparePassword(req.body.password, userFound.password);
-      
+
           if (!isPasswordValid) {
             return res.status(401).json({ token: null, message: 'Contraseña inválida' });
           }
-      
+
           // Una vez autenticado, genera un nuevo token
           const token = jwt.sign({ id: userFound._id, role: userFound.role }, config.SECRET, {
             expiresIn: 86400
           });
-      
+
           res.json({ token, userFound: { _id: userFound._id, role: userFound.role } });
       } catch (error) {
         return res.status(500).json({ msg: 'Error interno del servidor', details: error.message });
@@ -76,7 +76,7 @@ const controllerAuth={
         } catch (error) {
             return res.status(500).json({ msg:"Error interno del servidor", details: error.message })
         }
-    }, 
+    },
 
     // send email Link For reset Password
     sendPasswordLink : async (req, res) => {
@@ -121,6 +121,29 @@ const controllerAuth={
           return res.status(401).json({ status: 401, message: "Usuario inválido." });
       }
     },
+    changePassword: async (req, res) => {
+      try{
+        const newPassword = req.body.password; //Get password request's body
+        const id = req.userId;
+        console.log(req.userId)
+        console.log(req.newPassword)
+        //Created a new alt
+        const saltRounds = 10;
+        const salt = await bcrypt.genSalt(saltRounds);
 
+        // cryptinh new password using new salt 
+        const hashedPassword = await bcrypt.hash(newPassword, salt)
+      
+        const user = await User.findByIdAndUpdate(
+          {_id:id},
+          {password: hashedPassword}
+        );
+
+        res.status(201).json({message: "Password changed"})
+      
+      }catch(error){
+        res.status(401).json({status:401, error:"Server error"});
+      }
+    }
 }
 module.exports = controllerAuth
