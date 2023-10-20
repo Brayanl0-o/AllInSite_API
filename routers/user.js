cc50e113c2238f1a89003ab379db122d9a37fb54
Brayan <brayantandap@gmail.com>
2023-10-20 02:33:33 -0500

    Updated

--- routers/user.js
+++ routers/user.js
@@ -5,7 +5,7 @@ const router = express.Router()
 router.post('/create', controllerUser.create)
 router.get('/', controllerUser.getUser)
 router.get('/:id', controllerUser.getUserById)
-router.patch('/update/:id', controllerUser.updateUser)
+router.put('/update/:id', controllerUser.updateUser)
 router.delete('/delete/:id', controllerUser.deleteUser)
 
 module.exports = router
