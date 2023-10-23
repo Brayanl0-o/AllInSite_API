dd22fe49de7e6856ca197927e99eef22f16f3e47
Brayan <brayantandap@gmail.com>
2023-10-22 19:20:14 -0500

    Optimize

--- controllers/upload.js
+++ controllers/upload.js
@@ -1,6 +1,10 @@
 const multer = require('multer')
-
-
+const sharp = require('sharp')
+const helperImg =(filePath,fileName, size = 400) => {
+    return sharp(filePath)
+        .resize(size)
+        .toFile(`./optimize/${fileName}`)
+}
 const storage = multer.diskStorage({
     destination: (req, file, cb) =>{
         cb(null, './uploads')
@@ -16,5 +20,9 @@ const upload = multer({storage})
 exports.upload = upload.single('myFile')
 
 exports.uploadFile = (req, res) => {
+    helperImg(req.file.path,`micro-resize-${req.file.filename}`, 20);
+    helperImg(req.file.path,`small-resize-${req.file.filename}`, 100); 
+    helperImg(req.file.path,`medium-resize-${req.file.filename}`, 500);
+    helperImg(req.file.path,`large-resize-${req.file.filename}`, 1000);
     res.send({data: 'Imagen cargada'})
 }
