c7017b48285932b8593b611a5864f96e0a6e8d2e
Brayan <brayantandap@gmail.com>
2023-10-29 18:14:29 -0500

    Updated > uploadGame.js

--- controllers/videogames/uploadGame.js
+++ controllers/videogames/uploadGame.js
@@ -2,11 +2,11 @@ const multer = require('multer')
 const sharp = require('sharp')
 const fs = require('fs')
 const Game = require('../../models/game')
-const helperImg =(filePath,fileName, size = 400) => {
-    return sharp(filePath)
-        .resize(size)
-        .toFile(`./optimize/videogames/${fileName}`)
-}
+// const helperImg =(filePath,fileName, size = 400) => {
+//     return sharp(filePath)
+//         .resize(size)
+//         .toFile(`./optimize/videogames/${fileName}`)
+// }
 const storage = multer.diskStorage({
     destination: (req, file, cb) =>{
         cb(null, './uploads/videogames')
@@ -36,6 +36,6 @@ exports.uploadFile = (req, res) => {
 
     // helperImg(req.file.path,`small-resize-${req.file.filename}`, 400); 
     // helperImg(req.file.path,`medium-resize-${req.file.filename}`, 1000);
-    helperImg(req.file.path,`large-resize-${req.file.filename}`, 1600);
+    // helperImg(req.file.path,`large-resize-${req.file.filename}`, 1600);
     res.send({data: 'Imagen cargada'})
 }
