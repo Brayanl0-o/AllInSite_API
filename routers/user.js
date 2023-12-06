b02a021b331a8bb921da1c26558eaf03cb0be64e
Brayan <brayantandap@gmail.com>
2023-12-06 18:54:05 -0500

    updated > user.js (routers)

--- routers/user.js
+++ routers/user.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const controllerUser = require('../controllers/user')
 const router = express.Router()
+const controllerUploadUser = require('../controllers/uploadUser');
 
 router.post('/create', controllerUser.create)
 router.get('/', controllerUser.getUser)
 router.get('/:id', controllerUser.getUserById)
-router.put('/update/:id', controllerUser.updateUser)
+router.put('/update/:id',controllerUploadUser.upload, controllerUser.updateUser)
 router.delete('/delete/:id', controllerUser.deleteUser)
 
 module.exports = router
