a487eda1cbc8631a2e4a315e10a3eb90aefe977e
Brayan <brayantandap@gmail.com>
2023-12-08 17:53:44 -0500

    updated > uploadUser.js (routers)

---
+++ routers/users/uploadUser.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const controllerUploadUser = require('../../controllers/users/uploadUser')
+const router = express.Router();
+
+
+router.post('/users',controllerUploadUser.upload,controllerUploadUser.uploadFile);
+
+module.exports = router
