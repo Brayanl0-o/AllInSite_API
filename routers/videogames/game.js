644f8c6ad63035b10e0577f399cf115638063342
Brayan <brayantandap@gmail.com>
2023-12-13 00:05:38 -0500

    updated > game.js(routers)

--- routers/videogames/game.js
+++ routers/videogames/game.js
@@ -2,12 +2,16 @@ const express = require('express')
 const controllerGame = require('../../controllers/videogames/game')
 const router = express.Router()
 const controllerUploadGame = require('../../controllers/videogames/uploadGame');
+const validateRoles = require('../../middlewares/verifyRole')
+const authJwt = require('../../middlewares/authJwt')
+
+
 router.get('/filter', controllerGame.filterGames);
 router.post('/create',controllerUploadGame.upload, controllerGame.create)
 router.put('/update/:id', controllerUploadGame.upload, controllerGame.updateGame)
 router.get('/', controllerGame.getGame)
 router.get('/:id', controllerGame.getGameById)
-router.delete('/delete/:id', controllerGame.deleteGame)
+router.delete('/delete/:id',  authJwt.verifyToken,validateRoles, controllerGame.deleteGame)
 
 
 module.exports = router
