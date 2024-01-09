7e1d090c6f64ddd3ab48eea5a17520dd07104df0
Brayan <brayantandap@gmail.com>
2024-01-09 11:34:06 -0500

    updated > uploadGame.js (controllers)

--- routers/videogames/uploadGame.js
+++ routers/videogames/uploadGame.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const controllerUploadGame = require('../../controllers/videogames/uploadGame');
-const router =  express.Router();
+// const express = require('express');
+// const controllerUploadGame = require('../../controllers/videogames/uploadGame');
+// const router =  express.Router();
 
 
-router.post('/videogames',controllerUploadGame.upload,controllerUploadGame.uploadFile);
+// router.post('/videogames',controllerUploadGame.upload,controllerUploadGame.uploadFile);
 
 
-module.exports = router
+// module.exports = router
