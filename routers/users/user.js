3a46e7ce8bef0ece1283f136d4c9884c0a15cd9c
Brayan <brayantandap@gmail.com>
2024-01-09 11:28:33 -0500

    updated > user.js(routers)

--- routers/users/user.js
+++ routers/users/user.js
@@ -6,7 +6,8 @@ const controllerUploadUser = require('../../controllers/users/uploadUser');
 router.post('/create', controllerUser.create)
 router.get('/', controllerUser.getUser)
 router.get('/:id', controllerUser.getUserById)
-router.put('/update/:id',controllerUploadUser.upload, controllerUploadUser.uploadFile, controllerUser.updateUser)
+router.patch('/update/:id',controllerUser.updateUser)
+router.patch('/updateImg/:id',controllerUploadUser.upload, controllerUploadUser.uploadFile, controllerUser.updateUser)
 router.delete('/delete/:id', controllerUser.deleteUser)
 
 module.exports = router
