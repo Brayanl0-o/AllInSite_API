8d79ed47a1a73f0659b673cbf4f35f851415fbb0
Brayan <brayantandap@gmail.com>
2024-01-14 18:24:46 -0500

    updated > game.js (router)

--- routers/videogames/game.js
+++ routers/videogames/game.js
@@ -9,7 +9,7 @@ const authJwt = require('../../middlewares/authJwt')
 router.get('/filter', controllerGame.filterGames);
 router.post('/create',authJwt.verifyToken, validateRoles, controllerUploadGame.upload, controllerUploadGame.uploadFile, controllerGame.create)
 router.patch('/update/:id', authJwt.verifyToken, validateRoles, controllerGame.updateGame)
-router.patch('/updatedGameImg/:id', controllerUploadGame.upload, controllerUploadGame.uploadFile,controllerGame.updateGame )
+router.patch('/updatedGameImg/:id', authJwt.verifyToken, validateRoles,controllerUploadGame.upload, controllerUploadGame.uploadFile,controllerGame.updateGame )
 router.get('/', controllerGame.getGame)
 router.get('/:id', controllerGame.getGameById)
 router.delete('/delete/:id',  authJwt.verifyToken,validateRoles, controllerGame.deleteGame)
