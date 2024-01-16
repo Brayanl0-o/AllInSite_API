a14f6b752f2cf956a0c0deab26949a20f6b1627b
Brayan <brayantandap@gmail.com>
2024-01-15 21:55:23 -0500

    updated > user.js (routers)

--- routers/users/user.js
+++ routers/users/user.js
@@ -2,12 +2,14 @@ const express = require('express')
 const controllerUser = require('../../controllers/users/user')
 const router = express.Router()
 const controllerUploadUser = require('../../controllers/users/uploadUser');
+const validateRoles = require('../../middlewares/verifyRole')
+const authJwt = require('../../middlewares/authJwt')
 
 router.post('/create', controllerUser.create)
-router.get('/', controllerUser.getUser)
-router.get('/:id', controllerUser.getUserById)
-router.patch('/update/:id',controllerUser.updateUser)
-router.patch('/updateImg/:id',controllerUploadUser.upload, controllerUploadUser.uploadFile, controllerUser.updateUser)
-router.delete('/delete/:id', controllerUser.deleteUser)
+router.get('/', authJwt.verifyToken, validateRoles, controllerUser.getUser)
+router.get('/:id', authJwt.verifyToken, controllerUser.getUserById)
+router.patch('/update/:id', authJwt.verifyToken, controllerUser.updateUser)
+router.patch('/updateImg/:id', authJwt.verifyToken, controllerUploadUser.upload, controllerUploadUser.uploadFile, controllerUser.updateUser)
+router.delete('/delete/:id', authJwt.verifyToken, validateRoles, controllerUser.deleteUser)
 
 module.exports = router
