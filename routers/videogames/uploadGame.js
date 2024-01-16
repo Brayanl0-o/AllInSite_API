b570e89f4fe859b850f97a14d0e3e597f164adbf
Brayan <brayantandap@gmail.com>
2024-01-16 18:43:58 -0500

    delete

--- routers/videogames/uploadGame.js
+++
@@ -1,9 +0,0 @@
-// const express = require('express');
-// const controllerUploadGame = require('../../controllers/videogames/uploadGame');
-// const router =  express.Router();
-
-
-// router.post('/videogames',controllerUploadGame.upload,controllerUploadGame.uploadFile);
-
-
-// module.exports = router
