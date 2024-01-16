702e90d417670f303e0eb479165c976cb0d0cfcb
Brayan <brayantandap@gmail.com>
2024-01-16 18:28:52 -0500

    updated > app.js

--- app.js
+++ app.js
@@ -1,44 +1,44 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const path = require('path');
 
-const game = require('./routers/videogames/game')
-const user = require('./routers/users/user')
-const auth = require('./routers/auth')
-// const upload = require('./routers/upload')
-// const uploadImgGame = require('./routers/videogames/uploadGame')
-// const uploadImgUser = require('./routers/users/uploadUser')
+//Import routers
+const game = require('./routers/videogames/game');
+const user = require('./routers/users/user');
+const auth = require('./routers/auth');
 
-//Config midldlewares
+// Configure midldlewares
 app.use(cors({
     origin: "*",
     methods: "GET,HEAD,POST,PATCH,PUT,DELETE"
 }));
+
+// Serve images
 app.use('/uploads/videogames', express.static('uploads/videogames'));
 app.use('/uploads/users', express.static('uploads/users'));
 
 app.use(express.json());
 
+// Handle router upload images to change ext
 app.get('/uploads/:category/:filename.:ext', (req, res) => {
     const category = req.params.category;
     const filename = req.params.filename;
     const ext = req.params.ext;
   
-    // Verificar si la extensión no es .webp y redirigir a la versión .webp
+    // Check in if the extention is not '.wepb' and redirect to the version '.webp'
     if (ext !== 'webp') {
       res.redirect(`/uploads/${category}/${filename}.webp`);
     } else {
-      // Si la extensión ya es .webp, simplemente servir el archivo
+      // If the extention is .webp simply serve the file.
       res.sendFile(path.join(__dirname, `./uploads/${category}/${filename}.${ext}`));
     }
   });
 
-//router global
-// app.use('/upload', upload )
-// app.use('/uploadImgGame', uploadImgGame )
-app.use('/users', user)
-app.use('/games', game)
-app.use('/auth', auth)
-// app.use('/uploadImgUser', uploadImgUser )
+// Global router for accessing especific routers
+app.use('/users', user);
+app.use('/games', game);
+app.use('/auth', auth);
 
+// Exports the module (app)
 module.exports = app;
