8709a6d96b1413021ad3bf8d7bac4ec91abf6327
Brayan <brayantandap@gmail.com>
2024-01-16 18:28:37 -0500

    updated > index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const app = require('./app')
+//require to connect the db
 const database = require('./database')
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
 require(`dotenv`).config()
 
-const port = 3000 // conectec in port
+const port = 3000 // conecting in port
 app.listen(port, () => {
     console.log('Listening on: ' + 'http://localhost:' + port)
 });
