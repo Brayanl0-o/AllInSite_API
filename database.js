a151f64534f6459747fb4bc95118facf5b315662
Brayan <brayantandap@gmail.com>
2024-01-16 18:41:17 -0500

    updated > database.js

--- database.js
+++ database.js
@@ -1,18 +1,22 @@
 const mongoose = require('mongoose')
 const app = require('./app')
+
 require('dotenv').config()
 
+// Mode strict False
+mongoose.set('strict', false);
 
-mongoose.set("strictQuery", false);
+// Conect to bd using file 'MONGO_DB_URI' environment  variable
 mongoose.connect(process.env.MONGO_DB_URI + "/gameSite", {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}) // connect in mongoDB
+}) // msgg connect in mongoDB
     .then(() =>
-        console.log("Conexion to DB"))
+        console.log("Conexion Success to DB"))
     .catch((err) =>
         console.error(err));
 
+// Defining the route to indicate successful connection
 app.get('/', (req, res) => {
     res.send('Connected to API ALLIN')
 })
