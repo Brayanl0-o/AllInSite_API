1beaa75f4aa965731a0e42a720789c2888bb0979
Brayan <brayantandap@gmail.com>
2024-01-16 19:59:36 -0500

    updated > authJwt.ts

--- middlewares/authJwt.js
+++ middlewares/authJwt.js
@@ -2,20 +2,24 @@ const jwt = require('jsonwebtoken')
 const User = require('../models/user')
 const config =require('../config')
 
+//  Middleware to verify the token in the header 
 const authJwt ={
     verifyToken:async(req, res, next)=>{
+        // Check if the token is present in the header
         let token = req.headers['x-access-token']
-
         if(!token) return res.status(403).json({message:'no token'})
 
+        // Decode and verify the token using the config.SECRET to extract user data
         try{
             const decoded = jwt.verify(token,config.SECRET)
             req.userId = decoded.id
-            // console.log(decoded)
+
+            // Query the bd to check if the user exists
             const user = await User.findById(req.userId,{password:0})
-            // console.log(user)
             if(!user) return res.status(404).json({message:'no user found'})
+            
             req.userRoles = user.roles;
+
             next()
         }catch(error){
             res.status(401).json({message:'No autorizado'})
