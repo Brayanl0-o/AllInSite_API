ddf4a96e5bf037ece4375e35db08f07d3490f8c3
Brayan <brayantandap@gmail.com>
2024-01-16 19:47:03 -0500

    updated > users.js (routers)

--- routers/users/user.js
+++ routers/users/user.js
@@ -5,11 +5,18 @@ const controllerUploadUser = require('../../controllers/users/uploadUser');
 const validateRoles = require('../../middlewares/verifyRole')
 const authJwt = require('../../middlewares/authJwt')
 
+//  Router for user creation (currently is not in use)
 router.post('/create', controllerUser.create)
+
+//  Routers for getting users and user by Id
 router.get('/', authJwt.verifyToken, validateRoles, controllerUser.getUser)
 router.get('/:id', authJwt.verifyToken, controllerUser.getUserById)
+
+//  Routers for updating user's info, one router for updating user info and another for user Img
 router.patch('/update/:id', authJwt.verifyToken, controllerUser.updateUser)
 router.patch('/updateImg/:id', authJwt.verifyToken, controllerUploadUser.upload, controllerUploadUser.uploadFile, controllerUser.updateUser)
+
+//  Router for deleting a user (currently is not in use)
 router.delete('/delete/:id', authJwt.verifyToken, validateRoles, controllerUser.deleteUser)
 
 module.exports = router
