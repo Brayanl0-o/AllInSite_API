fba888706196060b8a54823ecebb83b547d54e2f
Brayan <brayantandap@gmail.com>
2024-01-16 19:59:51 -0500

    updated > verifyRole.js(middleware)

--- middlewares/verifyRole.js
+++ middlewares/verifyRole.js
@@ -1,15 +1,18 @@
 const validateRoles = (req, res, next) => {
+    // Define the allowed roles for accessing the route
     const allowedRoles = ['administrador'];
-  
+
+    // Extract roles from the user's token stored in req.userRoles
     const tokenRoles = req.userRoles;
-    // console.log('roles middleware',req.userRoles)
-   
+
+    // Check if roles are present and are an array
     if (!tokenRoles || !Array.isArray(tokenRoles)) {
       return res.status(400).json({ message: 'Los roles del token son requeridos' });
     }
   
-    // Iterar sobre los roles del token
+    // Iterate over the roles from the token
     for (const role of tokenRoles) {
+      // Check if the role is in the allowedRoles array
       if (!allowedRoles.includes(role)) {
         return res.status(400).json({ message: `Role ${role} no es válido.` });
       }
