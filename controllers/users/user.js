09d9d7468a19f77a5ce6e3f30fcc43d261cc4013
Brayan <brayantandap@gmail.com>
2024-01-19 17:07:37 -0500

    updated > user.js(controllers)

--- controllers/users/user.js
+++ controllers/users/user.js
@@ -7,8 +7,8 @@ const controllerUser ={
     create:async (req,res) =>{
         try{
             // Extract data de 'req.body'
-            const {firstName, lastName, userImg, email, password}= req.body
-            const userName = '${firstName}${lastName}'
+            const {firstName, lastName, userImg, email, password}= req.body;
+            const userName = '${firstName}${lastName}';
 
             // Check if email ready is in use 
             const existingUser = await User.findOne({ email });
@@ -16,7 +16,7 @@ const controllerUser ={
             if (existingUser) {
                 // If email existing, return an error
                 return res.status(400).json({ error: "El correo electrónico ya está en uso" });
-            }
+            };
 
             // Creating a new user's instance 
             const user = new User({
@@ -25,49 +25,49 @@ const controllerUser ={
                 userImg,
                 email, 
                 password
-            })
+            });
 
             // Using before instance for extract password and encrypt him
-            user.password = await User.encryptPassword(user.password)
+            user.password = await User.encryptPassword(user.password);
 
             // If everything is well, save the user
-            const savedUser = await user.save()
+            const savedUser = await user.save();
 
             // If everything is well, show menssage 'Created user'
-            return res.status(200).json({message:"Created user",user: savedUser})
+            return res.status(200).json({message:"Created user",user: savedUser});
         
         }catch(error){
             // If some left bad, show a error
-            return res.status(500).json({msg:"A server error occurred", details: error.message})
+            return res.status(500).json({msg:"A server error occurred", details: error.message});
         }
     },
     // Function for retrieving all users
     getUser:async(req, res)=>{
         try{
             // Retrieve all users from the database
-            const users = await User.find({})
+            const users = await User.find({});
 
             // Return the users in reverse order
-            res.json(users.reverse())
+            res.json(users.reverse());
         }catch(error){
             // If something goes wrong, show an error
-            return res.status(500).json({msg:"A server error occurred", details: error.message})
+            return res.status(500).json({msg:"A server error occurred", details: error.message});
         }
     },
     // Function for retrieving a user by ID
     getUserById: async(req, res) =>{
         try{
             // Retrieve the Id from 'req.params'
-            const {id}= req.params
+            const {id}= req.params;
 
             // Retrieve the user by Id from the database
-            const user = await User.findById(id)
+            const user = await User.findById(id);
 
             // Return the user in JSON format
-            res.json(user)
+            res.json(user);
         }catch(error){
             // If something goes wrong, show an error
-            return res.status(500).json({msg:error})
+            return res.status(500).json({msg:error});
         }
     },
     // Function for updating a user
@@ -115,19 +115,25 @@ const controllerUser ={
             res.status(200).json(updatedUser);
         }catch(error){
             // If something goes wrong, show an error
-            console.error('Error to update userImg  ',error)
+            console.error('Error to update userImg  ',error);
             res.status(500).json({ message: 'Error in User updated',msg:error });
         }
     },
+    // Function for deleting an user
     deleteUser:async(req, res)=>{
         try{
-            const {id}= req.params
-            await User.findByIdAndDelete(id)
-            res.json({msg:'Delete'})
+            // Retrieve the Id from 'req.params'
+            const {id}= req.params;
+
+            // Find and delete the user in the database
+            await User.findByIdAndDelete(id);
+            // Send a success message as a response
+            res.json({msg:'Delete'});
         }catch(error){
-            return res.status(500).json({msg:error})
+            // If something goes wrong, show an error
+            return res.status(500).json({msg:error});
         }
     }
 }
 
-module.exports = controllerUser
+module.exports = controllerUser;
