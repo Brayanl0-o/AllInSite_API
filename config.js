526a2fcee836595ad51cc6a84c1792562733de0c
Brayan <brayantandap@gmail.com>
2024-01-29 19:49:00 -0500

    updated

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports ={
-    SECRET:"clavesecret",
-    ADMIN_EMAIL : "brayanandrestriana07@gmail.com",
-    ADMIN_PASSWORD : "gvyo jkib uuqh omdn",
-    URL: "http://localhost:4200" 
-
-}
