c8f72840f60231bec5cb11d99689f25d145de317
Brayan <brayantandap@gmail.com>
2024-03-28 10:53:00 -0500

    feat > trailerGame (middleware)

---
+++ middlewares/trailerGame.js
@@ -0,0 +1,20 @@
+
+const urlFix = async(req, res, next) => {
+    const urlTrailer = req.body.gameTrailer;
+    if(urlTrailer){
+        const startIndex = urlTrailer.indexOf("watch?v=");
+        if(startIndex !== -1){
+            const endIndex = urlTrailer.indexOf("&", startIndex);
+            const videoId = urlTrailer.substring(startIndex + 8, endIndex !== -1 ? endIndex : undefined);
+            const newUrl =  "https://www.youtube.com/embed/" + videoId;
+            req.body.gameTrailer = newUrl;
+            // console.log('from inside',newUrl)
+            return next();
+        }else{
+            return res.json("Error to tranform url Trailer :c")
+        }
+    }else{
+        return res.json("Error dont arrive urlTrailer :c")
+    }
+}
+module.exports = urlFix;
