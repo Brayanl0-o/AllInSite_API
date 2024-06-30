714b7aaac97ddad9b833d9a85fefc842f09ebeb5
Brayan <brayantandap@gmail.com>
2024-06-30 18:39:59 -0500

    feat > song.js(controllers)

--- src/controllers/music/song.js
+++ src/controllers/music/song.js
@@ -1,4 +1,6 @@
 const Song = require('../../models/music/song');
+const User = require('../../models/user');
+const Playlist = require('../../models/music/playList.js');
 
 const controllerSong = {
     create: async(req,res) =>{
@@ -58,16 +60,109 @@ const controllerSong = {
             return res.status(500).json({msg:error});
         }
     },
+    createPlaylist: async (req, res) => {
+        try {
+            const { name } = req.body;
+            const userId = req.body.userId; 
+            console.log('',userId )
+
+            const newPlaylist = new Playlist({
+                name,
+                createdBy: userId
+            });
+
+            await newPlaylist.save();
+
+            const user = await User.findById(userId);
+            user.playlists.push(newPlaylist._id);
+            await user.save();
+
+            res.json(newPlaylist);
+        } catch (error) {
+            return res.status(500).json({ msg: error.message });
+        }
+    },
+    getPlaylists: async (req, res) => {
+
+        Playlist.find({}).then(playlists => {
+            console.log(playlists);
+        }).catch(error => {
+            console.error('Error:', error);
+        });
+        try {
+            const playlists = await Playlist.find({});
+            console.log(playlists);
+            res.json(playlists);
+        } catch (error) {
+            console.error('Error al obtener las listas de reproducción:', error);
+            return res.status(500).json({ msg: error.message, details: error });
+        }
+    },
+    addSongToPlaylist: async (req, res) => {
+        try {
+            const { playlistId, songId } = req.params;
+            const playlist = await Playlist.findById(playlistId);
+
+            if (!playlist) {
+                return res.status(404).json({ message: 'Playlist not found' });
+            }
+            if (!playlist.songs.includes(songId)) {
+                playlist.songs.push(songId);
+            }
+
+            await playlist.save();
+            res.json(playlist);
+        } catch (error) {
+            return res.status(500).json({ msg: error.message });
+        }
+    },
+    delteSongFromPlaylist: async (req, res) => {
+        try {
+            const { playlistId, songId } = req.params;
+
+            const updatedPlaylist  = await Playlist.findByIdAndUpdate(
+                playlistId,
+                { $pull: {songs: songId }},
+                { new: true }
+            );
+        
+            if (!updatedPlaylist) {
+                return res.status(404).json({ msg: 'Playlist not found' });
+            }
+    
+            res.json({ msg: 'Song removed from playlist', playlist: updatedPlaylist });
+        } catch (error) {
+            return res.status(500).json({ msg: error.message });
+        }
+    },
+    deletePlaylist:async (req,res)=> {
+        try {
+            const { id } = req.params;
+            const playlist = await Playlist.findById(id);
+
+            if (!playlist) {
+                return res.status(404).json({ msg: 'Playlist not found' });
+            }
+            
+            await Playlist.findByIdAndDelete(id);
+            await User.updateMany(
+                { playlists: id },
+                { $pull: { playlists: id } }
+            );
+            
+            res.json({ msg: 'Delete playlist' });
+        } catch (error) {
+            console.error('Error al eliminar la playlist:', error);
+            return res.status(500).json({ msg: error.message });
+        }
+    },   
     // Function for updating a Song
     updateSong:async(req, res)=>{
         try{
-
             // Retrieve the Id from 'req.params'
             const {id } = req.params;
-
             // Retrieve the Song data from 'req.body'
             const updatedSongData = req.body;
-
              // Find and update the Song in the database
              const updatedSong = await Song.findByIdAndUpdate(id, updatedSongData, {
                 new: true,
